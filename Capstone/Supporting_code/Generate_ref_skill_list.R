2036be43dee79a8c20a992ee2d39d6bd74b773de
MaelvanDijk <61116015+MaelvanDijk@users.noreply.github.com>
2022-03-24 14:56:19 +0100

    created a reference skill list generator

---
+++ Capstone/Supporting_code/Generate_ref_skill_list.R
@@ -0,0 +1,24 @@
+library(readr)
+library(tidyverse)
+
+# Read data containing skills
+indeed <- read_csv(".\\data\\indeed_job_dataset.csv", show_col_types = FALSE)
+
+
+# unnest the skills in  every row
+indeed <- indeed %>% 
+  mutate(Skill = strsplit(as.character(Skill), ",")) %>%
+  unnest(Skill)
+
+# Remove { and } and ' from the remaining skills 
+indeed$Skill <- gsub('[[]', '', indeed$Skill)
+indeed$Skill <- gsub('[]]', '', indeed$Skill)
+indeed$Skill <- gsub("[']", '', indeed$Skill)
+
+# remove whitespaces and remove duplicates
+skills <- table(indeed$Skill)
+skill_names <- names(skills)
+trimmed_skill_names<- trimws(skill_names, "left") %>% unique()
+
+# save the remaining skills to a txt file for later use. 
+write.table(trimmed_skill_names,"./Data_cleaned/D001/trimmed_skill_names.txt",sep="\t",row.names=FALSE, append = FALSE)
